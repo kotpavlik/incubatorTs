d80d7d95b62876b74325a91a442f7875c1ca2c51
kotpavlik <kotpavlik88@gmail.com>
2022-06-10 19:55:34 +0300

    Clock with arrow

--- src/components/clock/Clock.tsx
+++ src/components/clock/Clock.tsx
@@ -19,12 +19,74 @@ export const Clock = () => {
 
     }, [])
 
+
+
+    const secondRatio:number = Number(count.getSeconds()/60)
+    const minuteRatio:number = Number((secondRatio + count.getMinutes())/ 60)
+    const hourRatio:number = Number((minuteRatio + count.getHours())/12)
+debugger
     return (
         <div className={container_style.container}>
-            <div className={style.clock}>
+            <div className={style.clock_num}>
                 <span>{utilsForClock(count.getHours())}:</span>
                 <span>{utilsForClock(count.getMinutes())}:</span>
                 <span>{utilsForClock(count.getSeconds())} </span>
+            </div>
+            <div className={style.clock}>
+
+                <div className={`${style.hand} ${style.hour}`}
+                     style={
+                         {
+                             transform: `translate(-50%) rotate(${hourRatio * 360}deg)`
+                         }
+                     }></div>
+                <div className={`${style.hand} ${style.minute}`}
+                     style={
+                         {
+                             transform: `translate(-50%) rotate(${minuteRatio * 360}deg)`
+                         }
+                     }></div>
+                <div className={`${style.hand} ${style.second}`}
+                     style={
+                    {transform: `translate(-50%) rotate(${secondRatio * 360}deg)`}
+                }></div>
+
+                <div className={`${style.number} ${style.number1}`}>
+                    <div>1</div>
+                </div>
+                <div className={`${style.number} ${style.number2}`}>
+                    <div>2</div>
+                </div>
+                <div className={`${style.number} ${style.number3}`}>
+                    <div>3</div>
+                </div>
+                <div className={`${style.number} ${style.number4}`}>
+                    <div>4</div>
+                </div>
+                <div className={`${style.number} ${style.number5}`}>
+                    <div>5</div>
+                </div>
+                <div className={`${style.number} ${style.number6}`}>
+                    <div>6</div>
+                </div>
+                <div className={`${style.number} ${style.number7}`}>
+                    <div>7</div>
+                </div>
+                <div className={`${style.number} ${style.number8}`}>
+                    <div>8</div>
+                </div>
+                <div className={`${style.number} ${style.number9}`}>
+                    <div>9</div>
+                </div>
+                <div className={`${style.number} ${style.number10}`}>
+                    <div>10</div>
+                </div>
+                <div className={`${style.number} ${style.number11}`}>
+                    <div>11</div>
+                </div>
+                <div className={`${style.number} ${style.number12}`}>
+                    <div>12</div>
+                </div>
             </div>
         </div>
     );
